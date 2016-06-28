9e1824a17a29b2a81a53c1f7caae497239cd9ed3
Katelyn Medeiros <kmedeir1@bfx.win.ad.jhu.edu>
2016-06-27 21:34:05 -0400

    q2 commit

--- week04/q2.pl
+++ week04/q2.pl
@@ -2,3 +2,21 @@ package DNAgen;
 
   use strict;
   use warnings;
+
+use Exporter 'import';
+our @EXPORT_OK = ("random_sequence");
+
+sub random_sequence {
+    my( $length , $random_length ) = @_;
+  
+    if ( $random_length ) {
+      $length = int( rand( $length ));
+    }
+  
+    foreach ( 1 .. $length ) {
+      print $bases[int(rand(4))];
+    }
+    print "\n";
+  }
+
+1;
