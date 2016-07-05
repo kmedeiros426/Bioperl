e41d7e5e74e6b5f3f7972beb6854921eca526d06
Katelyn Medeiros <kmedeir1@bfx.win.ad.jhu.edu>
2016-07-04 22:57:29 -0400

    q1 comm

--- week05/q1.pl
+++ week05/q1.pl
@@ -9,13 +9,13 @@ my $arr2[$i] = <STDIN>;
 
 multarray(\@arr1, \@arr2);
 
-sub multaray{
+sub multarray{
    my ($ar1, $ar2)=@_;
    for(my $i=0; $i<3; $i++){
-      for(my $j=0l j<3; $j++){
+      for(my $j=0; j<3; $j++){
          my $x = $ar1->[$i]*$ar2->[$j];
          print $x."\t";
       }
-      print "\n:;
+      print "\n";
    }
 }
