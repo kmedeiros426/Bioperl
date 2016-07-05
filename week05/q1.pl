1dd4af8f85aa8ebb86cfa0d1820e588882dc5e78
Katelyn Medeiros <kmedeir1@bfx.win.ad.jhu.edu>
2016-07-04 22:44:59 -0400

    q1 comm

---
+++ week05/q1.pl
@@ -0,0 +1,21 @@
+use strict;
+use warnings;
+
+print "enter firt array";
+my $arr1[$i] = <STDIN>;
+
+print "enter second array";
+my $arr2[$i] = <STDIN>;
+
+multarray(\@arr1, \@arr2);
+
+sub multaray{
+   my ($ar1, $ar2)=@_;
+   for(my $i=0; $i<3; $i++){
+      for(my $j=0l j<3; $j++){
+         my $x = $ar1->[$i]*$ar2->[$j];
+         print $x."\t";
+      }
+      print "\n:;
+   }
+}
