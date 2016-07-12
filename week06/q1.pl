67373f2e4ae724378c45274d41a30a45de96868e
Katelyn Medeiros <kmedeir1@bfx.win.ad.jhu.edu>
2016-07-11 23:57:36 -0400

    comm 2

--- week06/q1.pl
+++ week06/q1.pl
@@ -9,28 +9,52 @@ use DBI;
 my $dbh = DBI->connect( "DBI:DQLite:dbname-dbfile" , "" , "" ,
    { PrintError => 0, RaiseError => 1} );
 
-DROP TABLE IF EXISTS mrna;
-CREATE TABLE mrna(
+$dbh->do("DROP TABLE IF EXISTS mrna");
+$dbh->do("CREATE TABLE mrna(
    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
    geneid INTEGER NOT NULL,
    tissue VARCHAR(255),
    explevel INTEGER,
    orf_start INTEGER,
-   orf_stop INTEGER
+   orf_stop INTEGER)"
 );
 
-DROP TABLE IF EXISTS gene;
-CREATE TABLE gene(
+$dbh->do("DROP TABLE IF EXISTS gene");
+$dbh->do("CREATE TABLE gene(
    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
    gene_name VARCHAR(255) NOT NULL,
    orgid INTEGER NOT NULL,
-   sequence TEXT
+   sequence TEXT)"
 );
 
-DROP TABLE IF EXISTS organism;
-CREAE TABLE organism(
+$dbh->do("DROP TABLE IF EXISTS organism");
+$dbh->do("CREATE TABLE organism(
    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-   orgname VARCHAR(255) NOT NULL.
+   orgname VARCHAR(255) NOT NULL)"
 );
 
- 
+my $fastafile = 'data.fasta';
+
+open (DATA, $fastafile);
+
+my $gene_name='';
+my $organism='';
+my $tissue='';
+my $start='';
+my $stop='';
+my $exp_level='';
+my $sequece='';
+
+while (<DATA>){
+   chomp;
+   if(/^>+/){
+      $gene_name =~ m/>(.+)\|;
+      $organism =~ m/>.+\|(.+)\|;
+      $tissue =~
+      $dbh->do("INSERT INTO gene VALUES(NULL,'$gene_name','$sequence'));
+      $dbh->do("INSERT INT0 organism VALUES(NULL,'$organism'));
+   }
+   else{
+      $sequence.=$_;
+   }
+
